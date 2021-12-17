c5166044d63ce978af5ca9c6ca886da9870ea53d
rhinoxD <coolshiv0721@gmail.com>
2021-12-17 19:11:33 +0530

    Routes Added in App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
 import './App.css';
 
 function App() {
-  return <div className='App'>PortFolio</div>;
+  return (
+    <div className='App'>
+      <Router>
+        <Route path='/' exact component={Home} />
+        <Route path='/projects' exact component={Projects} />
+        <Route path='/contact' exact component={Contact} />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
