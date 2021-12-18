2445fff67f08520301b444d91eebd91afd24627c
rhinoxD <coolshiv0721@gmail.com>
2021-12-18 09:56:49 +0530

    Intro Section Skeleton

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+import '../stylesheets/home.css';
+
 const Home = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <div
+        className='introduction flex-with-center'
+        style={{ backgroundImage: `url('./introbg.svg')` }}
+      >
+        <div>
+          <h1>SHIVAM</h1>
+          <div className='intro-content d-flex justify-content-between'>
+            <p>
+              MERN Stack <br /> Developer
+            </p>
+            <button className='primary-button'>Get Started</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
