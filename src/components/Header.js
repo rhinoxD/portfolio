43c16e533f44cc211b7e8a219415c774c211aff0
rhinoxD <coolshiv0721@gmail.com>
2021-12-18 11:29:58 +0530

    Header Styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { RiMenu3Fill, RiCloseFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   const [showHeader, setShowHeader] = useState(false);
@@ -16,6 +17,17 @@ const Header = () => {
           className='menu-item position-fixed top-0 end-0'
         />
       )}
+      <ul className={`${showHeader ? 'show-header' : 'hide-header'} n-box1`}>
+        <li>
+          <Link>Home</Link>
+        </li>
+        <li>
+          <Link>Projects</Link>
+        </li>
+        <li>
+          <Link>Contact</Link>
+        </li>
+      </ul>
     </div>
   );
 };
