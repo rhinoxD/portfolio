245cf8e0eb4135c30a920fd01fe60ff0411776cc
rhinoxD <coolshiv0721@gmail.com>
2022-01-24 10:24:58 +0530

    Edited Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,69 @@
-import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import Header from '../components/Header';
 
 const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios({
+      method: 'post',
+      url: 'contact',
+      data: { name, email, message },
+    }).then((response) => {
+      if (response.data.status === 'success') {
+        alert('Message Sent.');
+        resetForm();
+      } else if (response.data.status === 'fail') {
+        alert('Message failed to send.');
+      }
+    });
+    alert('Message Sent.');
+    resetForm();
+  };
+  const resetForm = () => {
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
   return (
     <div>
       <Header />
       <div className='container contact mt-5'>
         <div className='row pt-5'>
           <div className='col-md-12'>
-            <div className='contact-form m-3 p-5 n-box2'>
+            <form
+              onsubmit={handleSubmit}
+              className='contact-form m-3 p-5 n-box2'
+              method='post'
+            >
               <h3 className='font-bold'>Contact Me</h3>
               <hr />
-              <input type='text' className='form-control' placeholder='Name' />
+              <input
+                type='text'
+                className='form-control'
+                placeholder='Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
               <input
                 type='email'
                 className='form-control'
                 placeholder='Email'
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <textarea className='form-control' rows={3} defaultValue={''} />
-              <button className='primary-button mt-3'>SUBMIT</button>
-            </div>
+              <textarea
+                className='form-control'
+                rows={3}
+                defaultValue={''}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              />
+              <button className='primary-button submit mt-3'>SUBMIT</button>
+            </form>
           </div>
         </div>
       </div>
